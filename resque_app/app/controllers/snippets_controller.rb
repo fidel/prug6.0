960634053c92638859dfa41ec7a07e14eaf004a4
Szymon Fiedler <fidelski@gmail.com>
2011-11-23 23:50:06 +0100

    mostly working resque

--- resque_app/app/controllers/snippets_controller.rb
+++ resque_app/app/controllers/snippets_controller.rb
@@ -1,81 +1,29 @@
 class SnippetsController < ApplicationController
-  # GET /snippets
-  # GET /snippets.json
-  def index
-    @snippets = Snippet.all
 
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @snippets }
-    end
-  end
-
-  # GET /snippets/1
-  # GET /snippets/1.json
   def show
     @snippet = Snippet.find(params[:id])
 
     respond_to do |format|
       format.html # show.html.erb
-      format.json { render json: @snippet }
     end
   end
 
-  # GET /snippets/new
-  # GET /snippets/new.json
   def new
     @snippet = Snippet.new
 
     respond_to do |format|
       format.html # new.html.erb
-      format.json { render json: @snippet }
     end
   end
 
-  # GET /snippets/1/edit
-  def edit
-    @snippet = Snippet.find(params[:id])
-  end
-
-  # POST /snippets
-  # POST /snippets.json
   def create
     @snippet = Snippet.new(params[:snippet])
     if @snippet.save
-      uri = URI.parse('http://pygments.appspot.com/')
-      request = ::Net::HTTP.post_form(uri, {'lang' => @snippet.language, 'code' => @snippet.plain_code})
-      @snippet.update_attribute(:highlighted_code, request.body)
+      Resque.enqueue(::SnippetHighlither, @snippet.id)
       redirect_to @snippet, :notice => "Successfully created snippet."
     else
       render 'new'
     end
   end
 
-  # PUT /snippets/1
-  # PUT /snippets/1.json
-  def update
-    @snippet = Snippet.find(params[:id])
-
-    respond_to do |format|
-      if @snippet.update_attributes(params[:snippet])
-        format.html { redirect_to @snippet, notice: 'Snippet was successfully updated.' }
-        format.json { head :ok }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @snippet.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /snippets/1
-  # DELETE /snippets/1.json
-  def destroy
-    @snippet = Snippet.find(params[:id])
-    @snippet.destroy
-
-    respond_to do |format|
-      format.html { redirect_to snippets_url }
-      format.json { head :ok }
-    end
-  end
 end
--- resque_app/app/models/snippet.rb
+++ resque_app/app/models/snippet.rb
@@ -1,2 +1,7 @@
 class Snippet < ActiveRecord::Base
+
+  validates :name, :presence => true
+  validates :plain_code, :presence => true
+
 end
+
