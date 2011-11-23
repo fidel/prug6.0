249020219ad0c222bb7c08a7c9245629e5eab037
Szymon Fiedler <fidelski@gmail.com>
2011-11-23 21:31:03 +0100

    added empty presentation and app skeleton

---
+++ resque_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ResqueApp::Application.config.secret_token = 'b4bdd1b2e2e95823570e3c174865a504f548d8a81d89ac046c8373086072e251754ca226bd75d33513cac1c3c844c57c0dcd7f84f0693333310f90cf605b7282'
